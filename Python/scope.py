c8ce0ffd63e9fa00a90a9c0e87202dea275c3604
pullchocks <160859472+pullchocks@users.noreply.github.com>
2024-02-22 13:39:10 -0600

    Rename and scope add.

--- Python/scope.py
+++ Python/scope.py
@@ -1,13 +1,5 @@
-def modify_string(original):
-    original += " that has been modified."
-def modify_string_return(original):
-    original += " that has been modified."
-    return original
-
-test_string = "This is a test string"
-
-modify_string(test_string)
-print(test_string)
-
-test_string = modify_string_return(test_string)
-print(test_string)
+cont = False
+if cont:
+    var = 1234
+if var = !=0:
+        print(var)
