34b297d0a5a9fa1c1ddbfd0643860c05842b41a0
pullchocks <160859472+pullchocks@users.noreply.github.com>
2024-03-04 09:13:57 -0600

    Server

--- Chat/client.py
+++ Chat/client.py
@@ -50,4 +50,46 @@ class Server()
         self.echo("QUIT")
         
 client_thread.start()
-.
+
+class ClientListener(threading.Thread):
+    def __init__(self, server, socket, address):
+        super(ClientListener, self).__init__()
+        
+        self.server = server
+        self.client_address = address
+        self.client_socket = socket
+        self.listening = True
+        self.username = "No Username"
+        
+    def run(self)
+        while self.listening:
+            data = ""
+            try:
+                data = self.socket.recv(1024)
+            except socket.error:
+                print("Unable to receive data")
+                self.handle_msg(data)
+                time.sleep(0.1)
+            
+            print("Ending client thread for {0}".format(self.address))
+
+    def quit(self)
+        self.listening = False
+        self.socket.close()
+        self.server.remove_socket(self.socket)
+        self.server.echo("{0} has quit. \n".format(self.username))
+        
+    def handle_msg(self, data)
+        print("{0} sent: {1}".format(self.username, data))
+        username_result = re.search('^USERNAME (.*)$', data)
+                self.username = username_result.group(1)
+                self.server.echo("{0} has joined. \n".format(self.username))
+        elif data == "QUIT":
+            self.quit()
+    elif data == "":
+        else:
+            self.server.echo(data)
+            
+if __name__ == "__main__":
+    server = Server(59091)
+    server.run()
