0cc1cf4fdbe9a54834aff667c1c8c93bd47a166f
pullchocks <160859472+pullchocks@users.noreply.github.com>
2024-03-08 10:28:48 -0600

    Remove duplicate line

--- Graphics/canvas.py
+++ Graphics/canvas.py
@@ -1,3 +1,12 @@
 import turtle
 
 t = turtle.Turtle()
+
+t.forward(100)      # Move 100 pixels forward
+t.left(90)          # Turn 90 degrees to the left
+t.forward(100)      # Move 100 pixels forward
+t.left(90)          # Turn 90 degrees to the left
+t.forward(100)      # Move 100 pixels forward
+t.left(90)          # Turn 90 degrees to the left
+t.forward(100)      # Move 100 pixels forward
+t.left(90)          # Go back to the start
